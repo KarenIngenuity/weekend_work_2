51782a564eb69db1da33c05e4b04178cd8b5809d
Karen <karenjxliu@hotmail.com>
2016-07-10 23:31:46 -0500

    solved exercise, but not  without having created and fixed a 1.5-gb-eating infinite loop first.

--- algo/splitting_arrays.rb
+++ algo/splitting_arrays.rb
@@ -1,7 +1,14 @@
-# Complete the method called split_array, which two parameters, the first being an array, and the second being an integer. The method will then create an array of other arrays, each one being the size of the indicated by the second parameter. See the driver code for examples.
+# Complete the method called split_array, which has two parameters, the first being an array, and the second being an integer. The method will then create an array of other arrays, each one being the size indicated by the second parameter. See the driver code for examples.
 
 def split_array(array, array_size)
-
+  new_array = []
+  first_index = 0
+  new_array << array[0..array_size - 1]
+  until first_index + array_size - 1 == array.index(array.last)
+    first_index = first_index + array_size
+    new_array << array[first_index..(first_index + array_size - 1)]
+  end
+  return new_array
 end
 
 # Driver code - don't touch anything below this line.
